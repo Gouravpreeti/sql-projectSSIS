f81914f5b5dfe55dd1da599ca5c65daf26cf1886
unknown <gouravchaudhary860@gmail.com>
2025-02-19 13:26:53 +0530

    Some query add

--- SQLQuery64.sql
+++ SQLQuery64.sql
@@ -14,8 +14,8 @@ BEGIN
 	--SELECT @storeid,@prodidfk,@barcode
 	IF EXISTS (SELECT 1 FROM [dbo].[Tbl_barcode_Stock_Master] WHERE stkbar_storeidfk=@storeid AND stkbar_barcode=@barcode and stkbar_prodidfk=@prodidfk)
 	BEGIN
-	SELECT 'UPDATE'
-			
+	--SELECT 'UPDATE'
+			UPDATE Tbl_barcode_Stock_Master set Stkbar_Available=(select * from [dbo].[getstockcount_virtual_part_barcode](stkbar_prodidfk ,stkbar_barcode,stkbar_storeidfk,6611,'','CMP240003')) where  stkbar_storeidfk =@storeid AND stkbar_barcode=@barcode
 	END
 	ELSE
 	BEGIN
@@ -27,5 +27,15 @@ BEGIN
 END
 CLOSE boxwiseAvbl_cursor;
 DEALLOCATE boxwiseAvbl_cursor;
+
+select * from Tbl_ITO_Details where Itd_transferid='114255'
+
+select * from tbl_interstate_autoito  order by 1 desc
+
+delete  from Tbl_ITO_Details where itd_poid='128696'
+
+exec prc_erp_ito_final_update @qtype='UPDATEVALIDATE_ITO',@storeid='42',@user='U2412450402',@filename='file_name_example.txt',@filesize=2048,@date='',@json='{"ITONumber":"114255","StoreFromName":"42","StoreToName":"9","ProductID":"128696","ProductName":"Jordan Charcoal Velvet Corner Sofa","cbm":3.5,"oldcbm":3.5,"Quantity":"4","Warehousefrom":"NSW WH","Warehouseto":"Auburn","allqty":2,"isintersate":1,"EtaDate":"12/05/2025","boxdetails":"Standard","entrydate":"2025-02-19T04:41:46.093Z","vaildito":0,"isrowsno":"1608275","prod_type":0,"iseditchange":1,"isDelchange":0,"newquatity":"4"}',@company_id='CMP240003'
+
+
 
 
